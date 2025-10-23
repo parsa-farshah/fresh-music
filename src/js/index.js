7fa5fee709f138117b1b718b54009bb43787cce7
parsa <parsafarashah@gmail.com>
2025-10-24 02:48:20 +0330

    add backward and forward and timeLine

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,17 @@ let pauseBtn = document.querySelector("#pauseBtn");
 
 let musicItems = document.querySelectorAll("li");
 
+let _forward = document.querySelector("#forward");
+let _backward = document.querySelector("#backward");
+
 musicItems.forEach((item) => {
   item.addEventListener("click", () => {
+    let dataNumberMusicClickOn = "";
     // تمام آهنگ ها
     let _audioAll = document.querySelectorAll("audio");
-
     let MusicClickOn = item.querySelector("audio");
 
-    let dataNumberMusicClickOn = MusicClickOn.getAttribute("data-number");
-    console.log(dataNumberMusicClickOn);
+    dataNumberMusicClickOn = MusicClickOn.getAttribute("data-number");
 
     // آهنگ های قبلی pause میشن
     _audioAll.forEach((audio) => {
@@ -62,16 +64,61 @@ musicItems.forEach((item) => {
       pauseBtn.classList.add("hidden");
     });
 
-    let covers = document.querySelector(".slideNumber2");
+    _audio.addEventListener("timeupdate", () => {
+      let playLinePercent = (_audio.currentTime / _audio.duration) * 100;
+      let TimeLineMusic = document.querySelector("#TimeLineMusic");
+      TimeLineMusic.style.left = `${playLinePercent}%`;
+    });
+
+    // click update time line
+    let TimeLineMusicWrapper = document.querySelector("#TimeLineMusicWrapper");
+    TimeLineMusicWrapper.addEventListener("click", (e) => {
+      let widthWrapper = TimeLineMusicWrapper.clientWidth;
+      let click = e.offsetX;
+      let _durationWrapper = _audio.duration;
+
+      let newTimeClick = (click / widthWrapper) * _durationWrapper;
+
+      _audio.currentTime = newTimeClick;
+    });
+
+    let coverMusic1 = document.querySelector(".slideNumber");
+    let coverMusic2 = document.querySelector(".slideNumber2");
 
     // covers.forEach((cover) => {
     //   let slideNumbers = cover.getAttribute("data-number");
     // });
-
-    if (dataNumberMusicClickOn == 2) {
-      covers.classList.remove("z-40");
-      covers.classList.add("z-50");
+    if (dataNumberMusicClickOn == 1) {
+      coverMusic1.classList.remove("z-40");
+      coverMusic2.classList.remove("z-50");
+      coverMusic1.classList.add("z-50");
       console.log("hello");
     }
+    if (dataNumberMusicClickOn == 2) {
+      coverMusic2.classList.remove("z-40");
+      coverMusic2.classList.add("z-50");
+    }
+
+    // forward music
+    _forward.addEventListener("click", () => {
+      dataNumberForward = parseInt(dataNumberMusicClickOn) + 1;
+      if (dataNumberForward == 1) {
+        musicItems[0].click();
+      }
+      if (dataNumberForward == 2) {
+        musicItems[1].click();
+      }
+    });
+
+    // backward music
+    _backward.addEventListener("click", () => {
+      dataNumberForward = parseInt(dataNumberMusicClickOn) - 1;
+      if (dataNumberForward == 1) {
+        musicItems[0].click();
+      }
+      if (dataNumberForward == 2) {
+        musicItems[1].click();
+      }
+    });
   });
 });
